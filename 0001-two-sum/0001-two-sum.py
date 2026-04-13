c88c1ae3e3928b5c3ed47024837436e7f4e5dccc
Prince Kushwaha <251311931+princekushwaha9142@users.noreply.github.com>
2026-04-13 17:35:44 +0530

    Time: 0 ms (100%), Space: 12.9 MB (96.05%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        mp = {}
+
+        for i, num in enumerate(nums):
+            complement = target - num
+
+            if complement in mp:
+                return  [mp[complement], i]
+
+            mp[num] = i
+
+
+        
