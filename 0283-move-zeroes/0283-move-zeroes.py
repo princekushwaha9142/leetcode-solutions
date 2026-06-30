31c00c71da891dc369471ce4fe319d4cc776b304
Prince Kushwaha <251311931+princekushwaha9142@users.noreply.github.com>
2026-06-30 15:54:28 +0530

    Time: 4 ms (79.74%), Space: 13.3 MB (87.88%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        if len(nums) == 1:
+            return
+        i = 0
+        while i < len(nums):
+            if nums[i] == 0:
+                break
+            i += 1
+        if i == len(nums):
+            return
+        j = i + 1
+
+        while j < len(nums):
+            if nums[j] != 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+            j += 1
